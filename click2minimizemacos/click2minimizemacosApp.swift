b2ec801a7dfc4c7eeedf51eb464b6f37205871c8
dankguy17 <anishmadishetty@gmail.com>
2026-04-09 21:11:28 -0700

    Initial Commit

---
+++ click2minimizemacos/click2minimizemacosApp.swift
@@ -0,0 +1,17 @@
+//
+//  click2minimizemacosApp.swift
+//  click2minimizemacos
+//
+//  Created by Anish Madishetty on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct click2minimizemacosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
